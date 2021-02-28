9824534a1b89f630029eb351cf9a256be5eb595d
Joshua Wright <40744027+Jwright707@users.noreply.github.com>
2021-02-28 14:18:16 -0800

    Added a welcome screen

--- app/src/main/java/com/example/github_java_project/MainActivity.java
+++ app/src/main/java/com/example/github_java_project/MainActivity.java
@@ -13,17 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onRegisterButtonClick(View view){
+    public void onLoginButtonClick(View view){
         //      R.id.txtMessage grabs the id from the txtMessage in the activity_main.xml
         //      R stands for resources
-        EditText firstName = findViewById(R.id.firstName);
-        EditText lastName = findViewById(R.id.lastName);
-        EditText email = findViewById(R.id.email);
-        TextView firstNameDisplay = findViewById(R.id.firstNameDisplay);
-        TextView lastNameDisplay = findViewById(R.id.lastNameDisplay);
-        TextView emailDisplay = findViewById(R.id.emailDisplay);
-        firstNameDisplay.setText("First Name: " + firstName.getText().toString());
-        lastNameDisplay.setText("Last Name: " + lastName.getText().toString());
-        emailDisplay.setText("Email: " + email.getText().toString());
+        EditText userEmail = findViewById(R.id.userEmail);
+        EditText userPassword = findViewById(R.id.userPassword);
+//        TextView firstNameDisplay = findViewById(R.id.firstNameDisplay);
+//        TextView lastNameDisplay = findViewById(R.id.lastNameDisplay);
+//        TextView emailDisplay = findViewById(R.id.emailDisplay);
+//        firstNameDisplay.setText("First Name: " + firstName.getText().toString());
+//        lastNameDisplay.setText("Last Name: " + lastName.getText().toString());
+//        emailDisplay.setText("Email: " + email.getText().toString());
     }
 }
